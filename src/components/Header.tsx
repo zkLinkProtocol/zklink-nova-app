f644f41dfa26aa81cc45e96e2e043f86453aa14f
leo-zklink <leo@zklink.org>
2024-03-04 16:16:18 +0800

    feat: update home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@ import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from '@nextui-
 import { Link, NavLink } from 'react-router-dom'
 import { useWeb3Modal } from '@web3modal/wagmi/react'
 import { useAccount } from 'wagmi'
-// import { Logo } from './Logo'
 import styled from 'styled-components'
 import { showAccount } from '@/utils'
 
@@ -10,11 +9,11 @@ const NavBox = styled.nav`
     a {
         color: #9ccbbd;
         font-family: Satoshi;
-        font-size: 16px;
+        font-size: 1rem;
         font-style: normal;
         font-weight: 500;
-        line-height: 22px; /* 137.5% */
-        letter-spacing: -0.5px;
+        line-height: 1.375rem; /* 137.5% */
+        letter-spacing: -0.03125rem;
         &.active {
             color: #fff;
         }
@@ -24,15 +23,24 @@ const NavBox = styled.nav`
 const LogoBox = styled.div`
     .logo-text {
         position: absolute;
-        left: 47px;
-        top: 10px;
+        left: 2.94rem;
+        top: 0.63rem;
         color: #fff;
         font-family: Satoshi;
-        font-size: 32px;
+        font-size: 2rem;
         font-style: normal;
         font-weight: 700;
-        line-height: 22px; /* 68.75% */
+        line-height: 1.375rem; /* 68.75% */
     }
+`
+
+const ButtonText = styled.span`
+    color: #03d498;
+    font-family: Heebo;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.5rem; /* 150% */
 `
 
 export default function Header() {
@@ -40,61 +48,71 @@ export default function Header() {
     const { address, isConnected } = useAccount()
 
     return (
-        <Navbar
-            className='fixed bg-transparent'
-            maxWidth='full'
-            isBlurred={false}>
-            <NavbarBrand className='flex items-end'>
-                {/* <Logo /> */}
+        <>
+            <Navbar
+                className='fixed px-[1.5rem] py-[1.75rem] bg-transparent'
+                maxWidth='full'
+                isBlurred={false}>
+                <NavbarBrand className='flex items-end'>
+                    {/* <Logo /> */}
 
-                <Link to='/'>
-                    <LogoBox className='relative'>
-                        <img
-                            src='/img/logo-zklink.svg'
-                            alt='logo'
-                        />
-                        <span className='logo-text'>zk.Link</span>
-                    </LogoBox>
-                </Link>
+                    <Link to='/'>
+                        <LogoBox className='relative'>
+                            <img
+                                className='w-[9rem] h-[2.41rem]'
+                                src='/img/logo-zklink.svg'
+                                alt='logo'
+                            />
+                            <span className='logo-text'>zk.Link</span>
+                        </LogoBox>
+                    </Link>
 
-                <NavBox className='ml-10'>
-                    <NavbarContent
-                        className='hidden sm:flex gap-4'
-                        justify='center'>
-                        <NavbarItem>
-                            <NavLink
-                                to='/airdrop'
-                                className='nav-link'>
-                                Airdrop
-                            </NavLink>
-                        </NavbarItem>
-                        <NavbarItem>
-                            <NavLink to='/leaderboard'>Leaderboard</NavLink>
-                        </NavbarItem>
-                        <NavbarItem>
-                            <NavLink to='/about'>About</NavLink>
-                        </NavbarItem>
-                        <NavbarItem>
-                            <NavLink to='/bridge'>Bridge</NavLink>
-                        </NavbarItem>
-                    </NavbarContent>
-                </NavBox>
-            </NavbarBrand>
+                    <NavBox className='ml-[3.5rem]'>
+                        <NavbarContent
+                            className='hidden sm:flex gap-[2.5rem]'
+                            justify='center'>
+                            <NavbarItem>
+                                <NavLink
+                                    to='/airdrop'
+                                    className='nav-link'>
+                                    Airdrop
+                                </NavLink>
+                            </NavbarItem>
+                            <NavbarItem>
+                                <NavLink to='/leaderboard'>Leaderboard</NavLink>
+                            </NavbarItem>
+                            <NavbarItem>
+                                <NavLink to='/about'>About</NavLink>
+                            </NavbarItem>
+                            <NavbarItem>
+                                <NavLink to='/bridge'>Bridge</NavLink>
+                            </NavbarItem>
+                        </NavbarContent>
+                    </NavBox>
+                </NavbarBrand>
 
-            <NavbarContent justify='end'>
-                <NavbarItem className='hidden lg:flex'>
-                    {/* <Button
+                <NavbarContent justify='end'>
+                    <NavbarItem className='hidden lg:flex'>
+                        {/* <Button
                         className='bg-blue-950'
                         onClick={() => web3Modal.open({ view: 'Networks' })}>
                         Network
                     </Button> */}
-                    <Button
-                        color='success'
-                        onClick={() => web3Modal.open()}>
-                        {isConnected ? showAccount(address) : 'Connect wallet'}
-                    </Button>
-                </NavbarItem>
-            </NavbarContent>
-        </Navbar>
+                        <Button
+                            className='bg-[#1D4138] text-[#03D498] px-4 flex justify-center items-center gap-[0.75rem]'
+                            disableAnimation
+                            onClick={() => web3Modal.open()}>
+                            <img
+                                width={20}
+                                height={20}
+                                src='/img/icon-wallet.svg'
+                                alt='icon'
+                            />
+                            <ButtonText>{isConnected ? showAccount(address) : 'Connect wallet'}</ButtonText>
+                        </Button>
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
+        </>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,180 @@
+import { useWeb3Modal } from '@web3modal/wagmi/react'
+import { useState } from 'react'
+import OTPInput from 'react-otp-input'
 import styled from 'styled-components'
+import '@/styles/otp-input.css'
+import { useDispatch } from 'react-redux'
+import { setInviteCode } from '@/store/modules/airdrop'
+import { useNavigate } from 'react-router-dom'
 
 const BgBox = styled.div`
-    /* position: fixed;
-    top: 0;
-    left: 0; */
-    min-height: 100%;
     width: 100%;
-    /* min-width: 1680px; */
-    /* background: url('/img/bg-home.png') no-repeat; */
-
+    min-height: 100%;
     background-image: image-set('/img/bg-home.png' 0.5x, '/img/bg-home.png' 1x, '/img/bg-home.png' 2x);
     background-repeat: no-repeat;
     background-size: cover;
     background-position: 50%;
+`
 
-    /* transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
-    transition-timing-function: cubic-bezier(.4,0,.2,1);
-    transition-duration: .15s; */
+const CardBox = styled.div`
+    border-radius: 1rem;
+    background: rgba(0, 0, 0, 0.08);
+    backdrop-filter: blur(15.800000190734863px);
+`
+const TitleBox = styled.h4`
+    color: #c2e2ff;
+    font-family: Satoshi;
+    font-style: normal;
+    font-weight: 900;
+    letter-spacing: -0.03125rem;
+`
+const SubTitleBox = styled.p`
+    color: #c6d3dd;
+    font-family: Satoshi;
+    font-style: normal;
+    line-height: 2rem; /* 133.333% */
+    letter-spacing: -0.03125rem;
+`
 
-    /* background-image: url('/img/bg-home.png');
-    background-position: center center;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    background-size: cover;
-    background-color: #464646; */
+const ConnectWalletText = styled.span`
+    color: #c6ddda;
+    text-align: center;
+    font-family: Satoshi;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.5rem; /* 150% */
+    letter-spacing: -0.03125rem;
+`
+
+const FooterText = styled.p`
+    color: #c2e2ff;
+    font-family: Satoshi;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
 `
 
 export default function Home() {
+    const web3Modal = useWeb3Modal()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const [{ otp, numInputs, separator, placeholder, inputType }, setConfig] = useState({
+        otp: '',
+        numInputs: 5,
+        separator: '',
+        placeholder: '',
+        inputType: 'text' as const,
+    })
+
+    const handleOTPChange = (otp: string) => {
+        setConfig((prevConfig) => ({ ...prevConfig, otp }))
+    }
+
+    const enterInviteCode = () => {
+        dispatch(setInviteCode(otp))
+        navigate('/airdrop')
+    }
+
     return (
-        <BgBox>
+        <BgBox className='relative pb-[13rem]'>
+            <div className='flex justify-between pt-[9.5rem] pl-[6.5rem] pr-[6.88rem]'>
+                <div>
+                    <CardBox className='py-8 w-[30rem]'>
+                        <TitleBox className='pl-[1.56rem] text-[2.5rem] leading-[3.5rem]'>
+                            Bridge to Earn Yield and token rewards on zkLink Nova.
+                        </TitleBox>
+                        <SubTitleBox className='mt-4 px-6 text-[1.5rem] leading-8'>
+                            The only Ethereum L2 with native yield for ETH and stablecoins. Airdrop now live.
+                        </SubTitleBox>
+                    </CardBox>
+                    <div className='mt-4'>
+                        <img
+                            src='/img/btn-join-early-access.png'
+                            className='cursor-pointer'
+                            onClick={() => navigate('/airdrop')}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <CardBox className='py-8 w-[21.625rem] flex flex-col items-center text-center'>
+                        <TitleBox className='text-[1.5rem] leading-[2rem]'>Enter Your Invite Code</TitleBox>
+                        <SubTitleBox className='mt-[0.75rem] text-[1rem] leading-[1.5rem]'>
+                            Enter Your Invite Code to participate the campaign
+                        </SubTitleBox>
+
+                        <div className='my-8'>
+                            <OTPInput
+                                inputStyle='inputStyle'
+                                numInputs={numInputs}
+                                onChange={handleOTPChange}
+                                renderSeparator={<span>{separator}</span>}
+                                value={otp}
+                                placeholder={placeholder}
+                                inputType={inputType}
+                                renderInput={(props) => <input {...props} />}
+                                shouldAutoFocus
+                            />
+                        </div>
+
+                        <div>
+                            <img
+                                src='/img/btn-enter-invite-code.png'
+                                className='cursor-pointer'
+                                onClick={enterInviteCode}
+                            />
+                        </div>
+
+                        <div className='mt-4'>
+                            <ConnectWalletText
+                                className='cursor-pointer text-[1rem]'
+                                onClick={() => web3Modal.open()}>
+                                Connect Wallet
+                            </ConnectWalletText>
+                        </div>
+                    </CardBox>
+                </div>
+            </div>
+
+            <div className='absolute bottom-0 py-[4.5rem] flex justify-between items-end pl-[6.5rem] pr-[8.94rem]  w-full'>
+                <div>
+                    <FooterText>TVL/$2,306,521,248</FooterText>
+                    <FooterText className='mt-4'>TOTAL USERS / 173,933</FooterText>
+                </div>
+                <div className='flex items-center gap-[1.25rem]'>
+                    <a href=''>
+                        <img
+                            src='/img/icon-medium.svg'
+                            className='w-[1.5rem] h-[1.5rem]'
+                        />
+                    </a>
+                    <a href=''>
+                        <img
+                            src='/img/icon-dc.svg'
+                            className='w-[1.5rem] h-[1.5rem]'
+                        />
+                    </a>
+                    <a href=''>
+                        <img
+                            src='/img/icon-tg.svg'
+                            className='w-[1.5rem] h-[1.5rem]'
+                        />
+                    </a>
+                    <a href=''>
+                        <img
+                            src='/img/icon-twitter.svg'
+                            className='w-[1.25rem] h-[1.25rem]'
+                        />
+                    </a>
+                    <a href=''>
+                        <img
+                            src='/img/icon-github.svg'
+                            className='w-[1.5rem] h-[1.5rem]'
+                        />
+                    </a>
+                </div>
+            </div>
         </BgBox>
     )
 }
