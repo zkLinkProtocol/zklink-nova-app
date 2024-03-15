88429b898375e6e26f222d3af7394e09401f77cd
leo <leo@zklink.org>
2024-03-15 22:50:48 +0800

    feat: update nova points style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,6 +8,7 @@ import {
   ModalBody,
   ModalContent,
   ModalHeader,
+  Skeleton,
   Tooltip,
   useDisclosure,
 } from "@nextui-org/react";
@@ -39,7 +40,7 @@ import {
 } from "@/api";
 import { useAccount, useChainId, useBalance, useSwitchChain } from "wagmi";
 import toast from "react-hot-toast";
-import { NOVA_CHAIN_ID, TWEET_SHARE_TEXT } from "@/constants";
+import { NOVA_CHAIN_ID } from "@/constants";
 import useNovaNFT, { NOVA_NFT_TYPE } from "@/hooks/useNFT";
 import classNames from "classnames";
 import { useDispatch, useSelector } from "react-redux";
@@ -539,92 +540,112 @@ export default function Dashboard() {
             </Tooltip>
           </CardBox>
 
-          <CardBox className="mt-[1.5rem] p-[1.5rem]">
-            <p className="w-full text-[1rem] font-[700] text-[1rem] leading-[1.5rem] tracking-[0.06rem]">
-              Nova Points
-            </p>
-            <div className="flex items-center gap-[1rem]">
-              <span className="text-[2.5rem] font-[700]">
+          {(+accountPoint.novaPoint || 0) + (+accountPoint.referPoint || 0) ===
+          0 ? (
+            <CardBox className="mt-[1.5rem] p-[1.5rem] bg">
+              <p className="w-full text-[1rem] font-[700] text-[1rem] leading-[1.5rem] tracking-[0.06rem]">
+                Nova Points
+              </p>
+              <div className="py-[0.7rem] ml-[4rem] text-[2.5rem] font-[700]">
                 {formatNumberWithUnit(
                   (+accountPoint.novaPoint || 0) +
                     (+accountPoint.referPoint || 0)
                 )}
-              </span>
-              <Tooltip
-                className="p-[1rem]"
-                content={
-                  <p className="text-[1rem]">
-                    {getBooster(groupTvl) !== 0 &&
-                      `Group Booster: ${getBooster(groupTvl)}x`}
-                    <br />
-                    Early Bird Booster: 2x <br />
-                    <br />
-                    <a
-                      href="https://blog.zk.link/aggregation-parade-7997d31ca8e1"
-                      target="_blank"
-                      className="text-[#0bc48f]"
-                    >
-                      Learn More
-                    </a>
-                  </p>
-                }
-              >
-                <GreenTag className="py-[0.375rem] w-[5.625rem] text-[1rem]">
-                  {getBooster(groupTvl) + 2}x
-                </GreenTag>
-              </Tooltip>
-            </div>
-            {/* TODO: Est. in next epoch */}
-            {/* <p className="w-full text-[1rem] font-[700] text-[1rem] leading-[1.5rem] tracking-[0.06rem]">
+              </div>
+              <p className="text-[1rem] pr-4 opacity-60">
+                Don't worry, we are synchronizing Nova Points. Your Nova Points
+                will be updated after the syncing process has ended.
+              </p>
+            </CardBox>
+          ) : (
+            <CardBox className="mt-[1.5rem] p-[1.5rem]">
+              <p className="w-full text-[1rem] font-[700] text-[1rem] leading-[1.5rem] tracking-[0.06rem]">
+                Nova Points
+              </p>
+
+              <div className="flex items-center gap-[1rem]">
+                <span className="text-[2.5rem] font-[700]">
+                  {formatNumberWithUnit(
+                    (+accountPoint.novaPoint || 0) +
+                      (+accountPoint.referPoint || 0)
+                  )}
+                </span>
+                <Tooltip
+                  className="p-[1rem]"
+                  content={
+                    <p className="text-[1rem]">
+                      {getBooster(groupTvl) !== 0 &&
+                        `Group Booster: ${getBooster(groupTvl)}x`}
+                      <br />
+                      Early Bird Booster: 2x <br />
+                      <br />
+                      <a
+                        href="https://blog.zk.link/aggregation-parade-7997d31ca8e1"
+                        target="_blank"
+                        className="text-[#0bc48f]"
+                      >
+                        Learn More
+                      </a>
+                    </p>
+                  }
+                >
+                  <GreenTag className="py-[0.375rem] w-[5.625rem] text-[1rem]">
+                    {getBooster(groupTvl) + 2}x
+                  </GreenTag>
+                </Tooltip>
+              </div>
+              {/* TODO: Est. in next epoch */}
+              {/* <p className="w-full text-[1rem] font-[700] text-[1rem] leading-[1.5rem] tracking-[0.06rem]">
             +{accountPoint.referPoint}
           </p>
           <p className="text-[1rem] text-[#919192] font-[400]">
             Est. in next epoch
           </p> */}
 
-            <p className="flex justify-between items-center mt-[3rem] font-[400] text-[1rem] leading-[1.5rem] tracking-[0.06rem] text-[#919192]">
-              <div className="flex items-center gap-[0.5rem]">
-                <span>Earned By Your Deposit</span>
+              <p className="flex justify-between items-center mt-[3rem] font-[400] text-[1rem] leading-[1.5rem] tracking-[0.06rem] text-[#919192]">
+                <div className="flex items-center gap-[0.5rem]">
+                  <span>Earned By Your Deposit</span>
 
-                <Tooltip
-                  className="p-[1rem]"
-                  content={
-                    <p className="text-[1rem] max-w-[25rem] gap-[0.5rem]">
-                      This includes the initial deposit rewards as well as the
-                      ongoing rewards for holding (snapshot every 8 hours).
-                    </p>
-                  }
-                >
-                  <img
-                    src="/img/icon-info.svg"
-                    className="w-[0.875rem] h-[0.875rem] opacity-40"
-                  />
-                </Tooltip>
-              </div>
-              <span>{formatNumberWithUnit(accountPoint.novaPoint)}</span>
-            </p>
-            <p className="flex justify-between items-center mt-[1rem] font-[400] text-[1rem] leading-[1.5rem] tracking-[0.06rem] text-[#919192]">
-              <div className="flex items-center gap-[0.5rem]">
-                <span>Earned By Referring Friends</span>
+                  <Tooltip
+                    className="p-[1rem]"
+                    content={
+                      <p className="text-[1rem] max-w-[25rem] gap-[0.5rem]">
+                        This includes the initial deposit rewards as well as the
+                        ongoing rewards for holding (snapshot every 8 hours).
+                      </p>
+                    }
+                  >
+                    <img
+                      src="/img/icon-info.svg"
+                      className="w-[0.875rem] h-[0.875rem] opacity-40"
+                    />
+                  </Tooltip>
+                </div>
+                <span>{formatNumberWithUnit(accountPoint.novaPoint)}</span>
+              </p>
+              <p className="flex justify-between items-center mt-[1rem] font-[400] text-[1rem] leading-[1.5rem] tracking-[0.06rem] text-[#919192]">
+                <div className="flex items-center gap-[0.5rem]">
+                  <span>Earned By Referring Friends</span>
 
-                <Tooltip
-                  className="p-[1rem]"
-                  content={
-                    <p className="text-[1rem] max-w-[25rem] gap-[0.5rem]">
-                      The referral rewards will be updated every 8 hours along
-                      with the deposit rewards.
-                    </p>
-                  }
-                >
-                  <img
-                    src="/img/icon-info.svg"
-                    className="w-[0.875rem] h-[0.875rem] opacity-40"
-                  />
-                </Tooltip>
-              </div>
-              <span>{formatNumberWithUnit(accountPoint.referPoint)}</span>
-            </p>
-          </CardBox>
+                  <Tooltip
+                    className="p-[1rem]"
+                    content={
+                      <p className="text-[1rem] max-w-[25rem] gap-[0.5rem]">
+                        The referral rewards will be updated every 8 hours along
+                        with the deposit rewards.
+                      </p>
+                    }
+                  >
+                    <img
+                      src="/img/icon-info.svg"
+                      className="w-[0.875rem] h-[0.875rem] opacity-40"
+                    />
+                  </Tooltip>
+                </div>
+                <span>{formatNumberWithUnit(accountPoint.referPoint)}</span>
+              </p>
+            </CardBox>
+          )}
 
           <CardBox className="flex flex-col items-center mt-[1.5rem] p-[1.5rem]">
             <p className="w-full text-[1rem] font-[700] text-[1rem] leading-[1.5rem] tracking-[0.06rem] flex justify-between items-center">
