8c841510aa998228d05bb2782dbdae2b0d1cb4f1
leo <leo@zklink.org>
2024-03-17 17:10:04 +0800

    feat: add leaderboard table load more btn

--- src/components/PointsLeaderboard.tsx
+++ src/components/PointsLeaderboard.tsx
@@ -2,6 +2,8 @@ import { getAccountRank, getAccountsRank } from "@/api";
 import { TableColumnItem } from "@/types";
 import { formatNumberWithUnit } from "@/utils";
 import {
+  Button,
+  Spinner,
   Table,
   TableBody,
   TableCell,
@@ -45,23 +47,58 @@ export default function PointsLeaderboard() {
   ];
 
   const [data, setData] = useState<DataItem[]>([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoadMoreLoading, setIsLoadMoreLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  const limit = 50;
 
   const { address } = useAccount();
 
-  const getAccountsRankFunc = async () => {
-    let arr: DataItem[] = [];
-    const res = await getAccountsRank();
-    if (res?.result) {
-      arr = res.result;
+  const getAccountsRankFunc = async (isLoadMore = false) => {
+    if (isLoadMore) {
+      setIsLoadMoreLoading(isLoadMore);
+    } else {
+      setIsLoading(true);
     }
 
-    if (address) {
-      const accountRankRes = await getAccountRank(address);
-      if (accountRankRes?.result) {
-        arr.unshift(accountRankRes.result);
+    const nextPage = !isLoadMore ? 1 : page;
+
+    let arr: DataItem[] = [];
+
+    try {
+      const res = await getAccountsRank({
+        limit,
+        page: nextPage,
+      });
+      if (res?.result && Array.isArray(res.result) && res.result.length > 0) {
+        arr = res.result;
+        setPage(nextPage + 1);
       }
+
+      if (arr.length < limit) {
+        setHasMore(false);
+      }
+
+      if (address && nextPage === 1) {
+        const accountRankRes = await getAccountRank(address);
+        if (accountRankRes?.result) {
+          arr.unshift(accountRankRes.result);
+        }
+      }
+
+      if (isLoadMore) {
+        arr = data.concat(arr);
+      }
+
+      setData(arr);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+      setIsLoadMoreLoading(false);
     }
-    setData(arr);
   };
 
   const showAccount = (acc: any) => {
@@ -70,6 +107,7 @@ export default function PointsLeaderboard() {
   };
 
   useEffect(() => {
+    setPage(1);
     getAccountsRankFunc();
   }, [address]);
 
@@ -78,18 +116,42 @@ export default function PointsLeaderboard() {
       removeWrapper
       className="table min-h-[30rem]"
       classNames={{ thead: "table-header", tbody: "table-tbody" }}
+      bottomContent={
+        hasMore && !isLoading ? (
+          <div className="flex w-full justify-center">
+            <Button
+              isDisabled={isLoadMoreLoading}
+              variant="flat"
+              onPress={() => getAccountsRankFunc(true)}
+            >
+              {isLoadMoreLoading && <Spinner color="white" size="sm" />}
+              Load More
+            </Button>
+          </div>
+        ) : null
+      }
     >
       <TableHeader columns={columns}>
         {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
       </TableHeader>
 
-      <TableBody items={data}>
+      <TableBody
+        items={data}
+        isLoading={isLoading}
+        loadingContent={<Spinner label="Loading..." />}
+      >
         {data.map((item: any, index: number) =>
           index === 0 &&
           item?.address?.toLowerCase() === address?.toLowerCase() ? (
-            <TableRow key={index} className="border-b-1 border-slate-600">
+            <TableRow
+              key={index}
+              className="self-data border-b-1 border-slate-600"
+            >
               <TableCell>{item.rank}</TableCell>
-              <TableCell>{showAccount(item.address)} <span className="ml-[0.5rem]">(Your Address)</span></TableCell>
+              <TableCell>
+                {showAccount(item.address)}{" "}
+                <span className="ml-[0.5rem]">(Your Address)</span>
+              </TableCell>
               <TableCell>{showAccount(item.inviteBy)}</TableCell>
               <TableCell>
                 {formatNumberWithUnit(
