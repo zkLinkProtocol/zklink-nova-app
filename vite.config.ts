fa6d6117e595453dbea3d3da488c76c105939269
leo <leo@zklink.org>
2024-03-18 17:35:29 +0800

    update http proxy

--- vite.config.ts
+++ vite.config.ts
@@ -23,13 +23,13 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/twitter/, ""),
       },
       "/api": {
-        target: "https://app.zklink.io",
+        target: "https://goerli.app.zklink.io",
         // target: "https://app.zklink.io",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
       "/points": {
-        target: "https://app.zklink.io",
+        target: "https://goerli.app.zklink.io",
         // target: "https://app.zklink.io",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/points/, "/points"),
