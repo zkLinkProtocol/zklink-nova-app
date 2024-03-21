56c74d9eda0be629fe361967b51e026d0d9ceb67
leo <leo@zklink.org>
2024-03-21 14:00:32 +0800

    chore: rm useless

--- src/components/AssetsTable.tsx
+++ src/components/AssetsTable.tsx
@@ -446,28 +446,6 @@ export default function AssetsTable(props: IAssetsTableProps) {
     setFilterTableList(arr);
   }, [tableList, serachValue, isMyHolding, assetsTabsActive]);
 
-  const styles = {
-    label: "text-black/50 dark:text-white/90",
-    input: [
-      "bg-transparent",
-      "text-black/90 dark:text-white/90",
-      "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-    ],
-    innerWrapper: "bg-transparent",
-    inputWrapper: [
-      "shadow-xl",
-      "bg-default-200/50",
-      "dark:bg-default/60",
-      "backdrop-blur-xl",
-      "backdrop-saturate-200",
-      "hover:bg-default-200/70",
-      "focus-within:!bg-default-200/50",
-      "dark:hover:bg-default/70",
-      "dark:focus-within:!bg-default/60",
-      "!cursor-text",
-    ],
-  };
-
   return (
     <>
       <CardBox className="mt-[2rem] p-[0.38rem] flex justify-between items-center w-full overflow-auto">
