1d54deda7ec2b8af65a29148923f83540e2c617a
MickWang <1244134672@qq.com>
2024-04-06 18:32:32 +0800

    update nft draw animation for phase2

--- src/components/DrawAnimationV2/index.tsx
+++ src/components/DrawAnimationV2/index.tsx
@@ -125,7 +125,7 @@ const LotteryAnimation = React.forwardRef<Ref, IProps>((props, ref) => {
             <div
               key={item.name}
               className={`lottery-item ${
-                currentImageIndex === index + 1 ? "active" : ""
+                currentImageIndex === index  ? "active" : ""
               }`}
             >
               <div className="img-bg">
