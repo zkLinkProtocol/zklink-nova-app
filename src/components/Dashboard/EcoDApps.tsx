8fe1df15d3f8f80cec16fc318eddb4cef77b71fe
leo <leo@zklink.org>
2024-04-08 14:38:04 +0800

    feat: update layerbank

--- src/components/Dashboard/EcoDApps.tsx
+++ src/components/Dashboard/EcoDApps.tsx
@@ -1,7 +1,17 @@
 import { CardBox } from "@/styles/common";
 import styled from "styled-components";
-import { Tooltip as ReactTooltip } from "react-tooltip";
+// import { Tooltip as ReactTooltip } from "react-tooltip";
 import { AiFillCaretUp } from "react-icons/ai";
+import {
+  Button,
+  Checkbox,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
+import { useState } from "react";
 
 const Tag = styled.span`
   border-radius: 0.375rem;
@@ -44,110 +54,176 @@ const SubTh = styled.div`
 `;
 
 export default function EcoDApps() {
+  const warningModal = useDisclosure();
+  const [recognize, setRecognize] = useState(false);
+
   return (
-    <CardBox className="mt-[1.5rem] min-h-[30rem] min-w-[820px]">
-      <div>
-        <div className="px-[1.5rem] py-[0.5rem] rounded-[1rem] bg-[#081A23] flex items-center">
-          {/* {["Name", "Type", "Your Earned"].map((item, index) => (
+    <>
+      <CardBox className="mt-[1.5rem] min-h-[30rem] min-w-[820px]">
+        <div>
+          <div className="px-[1.5rem] py-[0.5rem] rounded-[1rem] bg-[#081A23] flex items-center">
+            {/* {["Name", "Type", "Your Earned"].map((item, index) => (
           <Th key={index}>{item}</Th>
         ))} */}
 
-          <Th>Name</Th>
-          <Th>Type</Th>
-          <Th className="flex items-center gap-1">
-            <span>Your Earned</span>
+            <Th>Name</Th>
+            <Th>Type</Th>
+            <Th className="flex items-center gap-1">
+              <span>Your Earned</span>
 
-            <img
-              data-tooltip-id="your-eraned"
-              src="/img/icon-info.svg"
-              className="w-[0.875rem] h-[0.875rem] opacity-40"
-            ></img>
-          </Th>
-        </div>
+              <img
+                data-tooltip-id="your-eraned"
+                src="/img/icon-info.svg"
+                className="w-[0.875rem] h-[0.875rem] opacity-40"
+              ></img>
+            </Th>
+          </div>
 
-        {/* <ReactTooltip
+          {/* <ReactTooltip
         id="your-eraned"
         place="top"
         style={{ fontSize: "14px", borderRadius: "16px" }}
         content="More points will be listed here soon."
       /> */}
 
-        <div className="px-[1.5rem] py-[1rem] flex items-center border-b-1 border-[#292A2A]">
-          <Td className="flex items-center gap-[0.5rem]">
-            <img
-              src="/img/icon-layerbank.svg"
-              className="w-[2.25rem] h-[2.25rem]"
-            />
+          <div className="px-[1.5rem] py-[1rem] flex items-center border-b-1 border-[#292A2A]">
+            <Td className="flex items-center gap-[0.5rem]">
+              <img
+                src="/img/icon-layerbank.svg"
+                className="w-[2.25rem] h-[2.25rem]"
+              />
+              <div>
+                <p
+                  className="text-[1rem] font-[700] flex items-center gap-1 cursor-pointer"
+                  onClick={() => warningModal.onOpen()}
+                >
+                  <span>LayerBank</span>
+                  <img
+                    src="/img/icon-open-in-new.svg"
+                    className="w-[1rem] h-[1rem]"
+                  />
+                </p>
+                <p className="text-[0.625rem] text-[#0AC18D] font-[700]">
+                  @LayerBankFi
+                </p>
+              </div>
+              <Tag className="px-[1rem] py-[0.12rem]">
+                <span className="text text-[#0bc48f]">2x boost</span>
+              </Tag>
+            </Td>
+            <Td>
+              <p className="text-[1rem] font-[700]">Lending</p>
+            </Td>
+            <Td className="flex justify-between items-center">
+              <div>
+                <p className="text-[1rem]">
+                  <span className="text-[#0BC48F] font-[700] whitespace-nowrap">
+                    14.2
+                  </span>{" "}
+                  Nova Points
+                </p>
+                <p className="text-[1rem] whitespace-nowrap">
+                  <span className="text-[#0BC48F] font-[700]">14.2</span>{" "}
+                  LayerBank Points
+                </p>
+              </div>
+              <Tag className="px-[1rem] py-[0.12rem] flex items-center gap-1">
+                <span className="text text-[#fff] whitespace-nowrap">
+                  How to Earn
+                </span>
+                <AiFillCaretUp />
+              </Tag>
+            </Td>
+          </div>
+        </div>
+        <div>
+          <div className="px-[1.5rem] py-[1rem] flex justify-between items-center">
             <div>
-              <p className="text-[1rem] font-[700] flex items-center gap-1">
-                <span>LayerBank</span>
+              <SubTh>Status</SubTh>
+              <p className="text-[0.875rem] ">Live</p>
+            </div>
+            <div>
+              <SubTh>Multiplier</SubTh>
+              <p className="text-[0.875rem]">2x Nova Points</p>
+            </div>
+            <div>
+              <SubTh>Description</SubTh>
+              <p className="max-w-[27.1875rem] text-[0.875rem] whitespace-wrap">
+                For each block that liquidity is in a pool you earn points
+                multiplied by the liquidity you provided
+              </p>
+            </div>
+            <div className="text-right">
+              <SubTh>Action</SubTh>
+              <p
+                className="text-[0.875rem] flex items-center gap-1 cursor-pointer"
+                onClick={() => warningModal.onOpen()}
+              >
+                <span>Provide Liquidity</span>
                 <img
                   src="/img/icon-open-in-new.svg"
                   className="w-[1rem] h-[1rem]"
                 />
               </p>
-              <p className="text-[0.625rem] text-[#0AC18D] font-[700]">
-                @LayerBankFi
-              </p>
             </div>
-            <Tag className="px-[1rem] py-[0.12rem]">
-              <span className="text text-[#0bc48f]">2x boost</span>
-            </Tag>
-          </Td>
-          <Td>
-            <p className="text-[1rem] font-[700]">Lending</p>
-          </Td>
-          <Td className="flex justify-between items-center">
-            <div>
-              <p className="text-[1rem]">
-                <span className="text-[#0BC48F] font-[700] whitespace-nowrap">
-                  14.2
-                </span>{" "}
-                Nova Points
-              </p>
-              <p className="text-[1rem] whitespace-nowrap">
-                <span className="text-[#0BC48F] font-[700]">14.2</span>{" "}
-                LayerBank Points
-              </p>
-            </div>
-            <Tag className="px-[1rem] py-[0.12rem] flex items-center gap-1">
-              <span className="text text-[#fff] whitespace-nowrap">
-                How to Earn
-              </span>
-              <AiFillCaretUp />
-            </Tag>
-          </Td>
+          </div>
         </div>
-      </div>
-      <div>
-        <div className="px-[1.5rem] py-[1rem] flex justify-between items-center">
-          <div>
-            <SubTh>Status</SubTh>
-            <p className="text-[0.875rem] ">Live</p>
-          </div>
-          <div>
-            <SubTh>Multiplier</SubTh>
-            <p className="text-[0.875rem]">2x Nova Points</p>
-          </div>
-          <div>
-            <SubTh>Description</SubTh>
-            <p className="max-w-[27.1875rem] text-[0.875rem] whitespace-wrap">
-              For each block that liquidity is in a pool you earn points
-              multiplied by the liquidity you provided
+      </CardBox>
+
+      <Modal
+        classNames={{ closeButton: "text-[1.5rem]" }}
+        className="bg-[#313841] max-w-[39rem] rounded-[1.5rem]"
+        size="lg"
+        isOpen={warningModal.isOpen}
+        onOpenChange={warningModal.onOpenChange}
+      >
+        <ModalContent className="p-2 mb-20 md:mb-0">
+          <ModalHeader>
+            <div className="text-center w-full flex justify-center items-center gap-1">
+              <img src="/img/icon-warning.svg" className="w-[2rem] h-[2rem]" />
+              <span className="text-[1.5rem] font-[500]">WARNING</span>
+            </div>
+          </ModalHeader>
+          <ModalBody>
+            <p className="text-[1.25rem] text-[#A0A5AD] font-[500] leading-[2rem]">
+              You are about to access a third-party website. Please do your own
+              research (DYOR) and avoid engaging in unfamiliar activities.
+              Please note that zkLink and its affiliates are not liable for any
+              losses, damages, or other consequences arising from your use of
+              third-party websites.
             </p>
-          </div>
-          <div className="text-right">
-            <SubTh>Action</SubTh>
-            <p className="text-[0.875rem] flex items-center gap-1 cursor-pointer">
-              <span>Provide Liquidity</span>
-              <img
-                src="/img/icon-open-in-new.svg"
-                className="w-[1rem] h-[1rem]"
+            <div className="mt-[1.88rem] flex items-center gap-2">
+              <input
+                type="checkbox"
+                id="recognize"
+                name="subscribe"
+                value="newsletter"
+                checked={recognize}
+                onChange={(e) => setRecognize(e.target.checked)}
               />
-            </p>
-          </div>
-        </div>
-      </div>
-    </CardBox>
+              <label
+                htmlFor="recognize"
+                className="text-[#A0A5AD] text-[0.875rem] flex-1"
+              >
+                I recognize the risks and will take responsibility for actions
+                on third-party websites.
+              </label>
+            </div>
+
+            <div className="py-[1rem]">
+              <Button
+                className="gradient-btn w-full h-[2.1875rem] flex justify-center items-center gap-[0.38rem] text-[1rem] tracking-[0.0625rem] flex-1"
+                disabled={!recognize}
+                onClick={() => {
+                  window.open("https://zklink.layerbank.finance/", "_blank");
+                }}
+              >
+                Continue to Access
+              </Button>
+            </div>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
