5fdf46613a553e39be7e7be96029e916e0535e44
leo <leo@zklink.org>
2024-04-08 10:57:25 +0800

    feat: add eco dapps

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -33,7 +33,6 @@ import { getCheckOkxPoints } from "@/utils";
 import NFTCard from "./components/NFTCard";
 import NFTCardV2 from "./components/NFTCardV2";
 import Decimal from "decimal.js";
-import EcoDApps from "@/components/Dashboard/EcoDApps";
 import {
   Button,
   Checkbox,
@@ -44,6 +43,7 @@ import {
   useDisclosure,
 } from "@nextui-org/react";
 import { setIsAdHide } from "@/store/modules/airdrop";
+import EcoDApps from "@/components/Dashboard/EcoDApps";
 
 const TabsBox = styled.div`
   .tab-item {
