9750a0dfc10bfcc98e04c830bf2760abc9e10732
leo <leo@zklink.org>
2024-07-10 16:13:21 +0800

    fix points tooltip title

--- src/components/DashboardS2/Tabs/EcoDApps.tsx
+++ src/components/DashboardS2/Tabs/EcoDApps.tsx
@@ -1395,7 +1395,7 @@ export default function EcoDApps({
               content={
                 <div className="min-w-[200px]">
                   <div className="text-[#999] text-[14px] font-[500]">
-                    Your Sector Points
+                    Sector Allocated Points
                   </div>
                   {categoryPointsTooltips.map((item, index) => (
                     <div
