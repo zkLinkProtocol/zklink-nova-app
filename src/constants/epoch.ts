3c4c5455c7517652bec82f9420392da183495276
leo <leo@zklink.org>
2024-12-11 21:14:41 +0800

    update for aggparade parade end

--- src/constants/epoch.ts
+++ src/constants/epoch.ts
@@ -7,11 +7,11 @@ export const epochList = [
   {
     name: "Epoch Two",
     season: 3,
-    time: "Snapshot taken on July 15, 2024, at 12:00 PM UTC",
+    time: "Snapshot taken on August 31, 2024, at 12:00 PM UTC",
   },
   {
     name: "Epoch Three",
     season: 4,
-    time: "Snapshot taken on July 15, 2024, at 12:00 PM UTC",
+    time: "Snapshot taken on December 13, 2024, at 12:00 PM UTC",
   },
 ];
